3d24f89fdbbfb3f98019a795526380b5eeffba44
kobrinsam <60291549+kobrinsam@users.noreply.github.com>
2023-01-01 15:08:00 -0500

    supabasedata
    added supabase as backend database

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,23 @@
+from supabase import create_client, Client
 import altair as alt
 import pandas as pd
 import streamlit as st
 import numpy as np
-import requests, zipfile, io
-from streamlit_lottie import st_lottie  # pip install streamlit-lottie
 import json
 import datetime as dt
+import requests
+url = "https://pmmghxxijwmbhfautdiz.supabase.co"
+key = st.secrets(['key'])
 
+#sql query
+supabase: Client = create_client(url, key)
+data = supabase.table("dcinbox3").select("*").execute()
+
+# Get the JSON data from the APIResponse object
+json_data = data.json()
+data = json.loads(json_data)
+data = data['data']
+data = pd.DataFrame(data)
 
 st.title("DC Inbox Dashboard")
 st.subheader("Official e-newsletters from every member of Congress. In one place. In real time.")
@@ -20,16 +31,23 @@ url= "https://www.dcinbox.com/api/csv.php"
 # data on computer "/Users/samkobrin/Downloads/dc_inbox_test_data.csv"
 # data in github
 #import data
+#data from supabase
+
+#assert len(data.data) > 0
 @st.experimental_memo
 def get_data():
-    df = pd.read_csv('dc_inbox_test_data.csv', parse_dates=['Date of Birth'])
-    df['Date'] = df['Unix Timestamp'].apply(lambda x: dt.datetime.fromtimestamp(x/1000))
+
+    #df = pd.read_csv('dc_inbox_test_data.csv', parse_dates=['Date of Birth'])
+    df = data
+    df['Unix Timestamp'] = df['Unix Timestamp'].apply(lambda x: int(x))
+    df['Date'] = pd.to_datetime(df['Unix Timestamp'],unit='s', origin='unix')
     df = df.drop(['Unix Timestamp'], axis=1)
     df['Full Text'] = df['Subject'] + ' ' +  df['Body']
     df['Full Name'] = df['First Name'] + ' ' + df['Last Name'] + ' (' + df['BioGuide ID'] + ')'
    
     return df
 df = get_data()
+st.dataframe(df)
 search = st.text_input(label= 'Search', placeholder = 'e.g. Healthcare')
 
 ### sidebar
@@ -46,14 +64,14 @@ with st.sidebar:
 
 filtered_df = df[(df['Party'].isin(party)) & (df['Chamber'].isin(chamber)) & (df.Gender.isin(gender)) & (df['Full Text'].str.contains(search, case = False))]
 
-chart_df = filtered_df.groupby(pd.Grouper(key = 'Date', freq='W')).agg({'ID':pd.Series.nunique}).reset_index()
-
+#chart_df = filtered_df.groupby(pd.Grouper(key = 'Date', freq='W')).agg({'ID':pd.Series.nunique}).reset_index()
+chart_df = filtered_df.groupby(pd.Grouper(key = 'Date', freq='H')).agg({'HateSpeechWordCount':pd.Series.sum}).reset_index()
 st.dataframe(filtered_df)
 st.dataframe(chart_df)
 st.subheader("Chart")
 chart = alt.Chart(chart_df).mark_line().encode(
 x='Date',
-y= alt.Y('ID', title = "Emails Sent"))
+y= alt.Y('HateSpeechWordCount', title = "Count of Divisive Words Used"))
 
 st.altair_chart(chart, use_container_width=True)
 
@@ -62,36 +80,4 @@ st.write("Data Sources: [DC Inbox By Professor Lindsey Cormack](https://www.dcin
 st.text('By Sam Kobrin')
 
 
-
-#strokeDash='Party',
-#tooltip=['Commodity', 'Amount', 'SC_Unit_Desc', 'Date']).interactive()
-    ###lottefile
-    # GitHub: https://github.com/andfanilo/streamlit-lottie
-    # Lottie Files: https://lottiefiles.com/
-
-def load_lottiefile(filepath: str):
-        with open(filepath, "r") as f:
-            return json.load(f)
-
-
-def load_lottieurl(url: str):
-        r = requests.get(url)
-        if r.status_code != 200:
-            return None
-        return r.json()
-    
-
-#lottie_coding = load_lottiefile("lottiefile.json")  # replace link to local lottie file
-lottie_wheat = load_lottieurl("https://assets1.lottiefiles.com/packages/lf20_t2xm9bsw.json")
-st_lottie(
-        lottie_wheat,
-        speed=1,
-        reverse=False,
-        loop=True,
-        quality="low", # medium ; high
-         # canvas
-        height=None,
-        width=None,
-        key=None,
-    )
 
